dbd46fd72889ebbc332ee2f32e7323da1872d466
Fanf-1997 <kenny19971108@gmail.com>
2020-12-01 20:04:01 +0000

    2020.12.01

--- exercise3.js
+++ exercise3.js
@@ -1,16 +1,17 @@
 // Exercise 3
-function sublist(nums) {
-  let res = []
-  let len = nums.length;
-    for (let i = 0; i < (1 << len); i++) {
-		let arr = [];
-        for (let j = 0; j < len; j++) {
-            if (i & (1 << j)) {
-              arr.push(nums[j]);
-          }
-        }
-        res.push(arr);
-    }
-    return res;
+function sublist(a) {
+  var result = []
+  for (var i = 0; i < a.length; i++){
+      var t = []
+      for (var j = i; j < a.length; j++){
+          t = t.concat(a[j])
+          result.push(t)
+      }
+  }
+  result.push([])
+  return result
+
 }
+
+
 console.log(sublist([1,2,3]))//如果有重复元素怎么办
--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-let a=[1,2,3,4]
-console.log(a[0]);
+function sublist(l) {
+    let L0 = []
+    for (let i=0;i<l.length;i++){
+        for (let j=0;j<L0.length;j++){
+            L0.concat(L0[j] + [l[i]])
+          }
+    console.log('List =', L0)
+
+}
+}
+console.log(sublist([1,2,3]))
